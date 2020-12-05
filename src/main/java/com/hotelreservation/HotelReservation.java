3b3268cdb079c46f9688a5075caceb76694200b0
ManaliParab12 <manaliparab10@gmail.com>
2020-12-05 09:47:04 +0530

    Added hotel with rates to hotel Reservation system

--- src/main/java/com/hotelreservation/HotelReservation.java
+++ src/main/java/com/hotelreservation/HotelReservation.java
@@ -1,7 +1,27 @@
 package com.hotelreservation;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HotelReservation {
+    Scanner scanner = new Scanner(System.in);
     public static void main (String[] args) {
         System.out.println("Welcome to Hotel");
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.addHotel();
+    }
+    ArrayList<Hotel> hotelList = new ArrayList();
+
+    public void addHotel(){
+        System.out.println("Enter Hotel Name");
+        String hotelName = scanner.next();
+        System.out.println("Enter Hotel Name");
+        String hotelType = scanner.next();
+        System.out.println("Enter Hotel Name");
+        int rates = scanner.nextInt();
+
+        Hotel hotel = new Hotel(hotelName, hotelType, rates);
+        hotelList.add(hotel);
+        System.out.println(hotelList);
     }
 }
