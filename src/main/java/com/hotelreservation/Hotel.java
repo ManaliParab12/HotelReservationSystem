14e2e5cb6ad6794205815db4b55e8771006570ee
ManaliParab12 <manaliparab10@gmail.com>
2020-12-09 17:26:01 +0530

    Added method to find cheapest hotel for given  Date range

--- src/main/java/com/hotelreservation/Hotel.java
+++ src/main/java/com/hotelreservation/Hotel.java
@@ -1,46 +1,40 @@
 package com.hotelreservation;
 
-public class Hotel {
-    private String hotelName;
-    private  String hotelType;
-    private int rates;
+public class Hotel implements Comparable<Hotel> {
+    private String name;
+    private float rate;
 
-    public Hotel(String hotelName, String hotelType, int rates) {
-        this.hotelName = hotelName;
-        this.hotelType = hotelType;
-        this.rates = rates;
+
+    public String getName() {
+        return name;
     }
 
-    public String getHotelName() {
-        return hotelName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setHotelName(String hotelName) {
-        this.hotelName = hotelName;
+    public float getRate() {
+        return rate;
     }
 
-    public String getHotelType() {
-        return hotelType;
+    public void setRate(float rate) {
+        this.rate = rate;
     }
 
-    public void setHotelType(String hotelType) {
-        this.hotelType = hotelType;
-    }
 
-    public int getRates() {
-        return rates;
-    }
-
-    public void setRates(int rates) {
-        this.rates = rates;
-    }
 
     @Override
     public String toString() {
         return "Hotel{" +
-                "hotelName='" + hotelName + '\'' +
-                ", hotelType='" + hotelType + '\'' +
-                ", rates=" + rates +
-                '}';
+                "name='" + name + '\'' +
+                ", rate=" + rate + '}';
     }
+
+
+    @Override
+    public int compareTo(Hotel h) {
+        return (int) (this.rate - h.rate);
+    }
+
+
 }
